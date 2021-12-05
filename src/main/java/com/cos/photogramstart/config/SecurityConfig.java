44d53856451ea64fc5ea9bae7faab0d6ff4a8ca8
sosow0212 <sosow0212@naver.com>
2021-12-05 22:42:55 +0900

    SecurityConfig 설정

---
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.cos.photogramstart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // 해당 파일로 시큐리티를 활성화 시킴
+@Configuration // IoC에 등록
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//      super.configure(http); // super.~ 삭제를 하면 기존 시큐리티가 가지고 있는 기능이 전부 비활성화된다.
+        http.authorizeRequests()
+                .antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**").authenticated() // 이 주소로 시작되면 인증이 필요하고
+                .anyRequest().permitAll() // 그게 아닌 모든 요청은 허용하겠다 라는 뜻임
+                .and()
+                .formLogin()
+                .loginPage("/auth/signin") // 위에 주소로 이동하면 /auth/signin 으로 자동 이동시킨다라는 뜻
+                .defaultSuccessUrl("/"); // 로그인이 정상적이면 "/" 이동
+    }
+}
---
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -0,0 +1,8 @@
+package com.cos.photogramstart.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AuthController {
+
+}
