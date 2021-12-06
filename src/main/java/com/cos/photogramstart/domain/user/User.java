7b3aee2dd1cea8583185f3c79f705e054b99b5f1
sosow0212 <sosow0212@naver.com>
2021-12-06 21:06:37 +0900

    BCrypt 사용한 암호화 적용 및 username 중복 방지

--- src/main/java/com/cos/photogramstart/domain/user/User.java
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -21,7 +21,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 데이터베이스를 따라간다.
     private int id;
 
-    @Column(unique = true) // username은 중복이 안된다.
+    @Column(length = 20, unique = true) // varchar(20), username은 중복이 안된다.
     private String username;
     private String password;
 
