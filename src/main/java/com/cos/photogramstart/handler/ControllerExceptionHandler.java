047479f9dbcff886d62a6adb0b6d37b5a533e751
sosow0212 <sosow0212@naver.com>
2021-12-08 20:35:23 +0900

    공통응답 DTO

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.cos.photogramstart.handler;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.web.dto.CMRespDto;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,8 @@ import java.util.Map;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class) // 런타임Exception이 발생하는 모든 Exception을 이 함수가 가로챈다
-    public Map<String, String> validationException(CustomValidationException e) {
-        return e.getErrorMap();
+    public CMRespDto<?> validationException(CustomValidationException e) {
+        // <?> 제네릭에서 물음표를 적으면서 어떤 타입의 값이 와도 알아서 바뀌게 된다.
+        return new CMRespDto<Map<String,String>>(-1, e.getMessage(), e.getErrorMap());
     }
 }
