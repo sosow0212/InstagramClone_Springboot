def22090331989c9330f8e9c5bf5c11b11f9053b
sosow0212 <sosow0212@naver.com>
2021-12-08 22:31:09 +0900

    로그인 처리 - 2 (오류)

--- src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
@@ -1,18 +1,33 @@
 package com.cos.photogramstart.config.auth;
 
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-
+@RequiredArgsConstructor // userRepository 부를 때 필요함
 @Service // IoC
 public class PrincipalDetailsService implements UserDetailsService {
 
+    private UserRepository userRepository;
 
 
+    // username만 받으면 password는 Spring-security가 알아서 맞춰서 처리해준다.
+    // return이 잘되면 자동으로 UserDetails 타입을 세션을 만들어준다. (즉 userEntity를 잘 받아오면)
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+        User userEntity = userRepository.findByUsername(username);
+
+        System.out.println(username + "/// " + userEntity);
+
+        if(userEntity == null) {
+            return null;
+        } else {
+            return new PrincipalDetails(userEntity);
+        }
     }
 }
