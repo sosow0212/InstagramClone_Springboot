6c1a3113283eb4ab898890f84d0e40271fe8b349
sosow0212 <sosow0212@naver.com>
2021-12-09 18:58:41 +0900

    세션 찾기

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,5 +1,7 @@
 package com.cos.photogramstart.web;
 
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +15,13 @@ public class UserController {
     }
 
     @GetMapping("/user/{id}/update")
-    public String update(@PathVariable int id) {
+    public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        /**
+         *  @AuthenticationPrincipal PrincipalDetails principalDetails
+         *  위에 어노테이션을 통해서 세션 정보를 바로 찾을 수 있다.
+         *  System.out.println("세션 정보 : " + principalDetails.getUser());
+         */
+
         return "user/update";
     }
 }
