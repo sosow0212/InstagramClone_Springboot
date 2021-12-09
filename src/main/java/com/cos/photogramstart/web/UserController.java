aead140be5bb7c90071d1ac50c6c2f17254deb6c
sosow0212 <sosow0212@naver.com>
2021-12-09 19:19:57 +0900

    태그 라이브러리

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -3,6 +3,7 @@ package com.cos.photogramstart.web;
 import com.cos.photogramstart.config.auth.PrincipalDetails;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -21,6 +22,9 @@ public class UserController {
          *  위에 어노테이션을 통해서 세션 정보를 바로 찾을 수 있다.
          *  System.out.println("세션 정보 : " + principalDetails.getUser());
          */
+
+//        model.addAttribute("principal", principalDetails.getUser());
+        // model을 안 넘겨줘도 security-taglibs 를 통해 세션 정보를 알아서 넘겨줄 수 있다.
 
         return "user/update";
     }
--- src/main/webapp/WEB-INF/views/layout/header.jsp
+++ src/main/webapp/WEB-INF/views/layout/header.jsp
@@ -2,6 +2,14 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags"%>
 
+<sec:authorize access="isAuthenticated()">
+	<sec:authentication property="principal" var="principal" />
+</sec:authorize>
+<%--
+property를 var이라는 이름으로 받고, 이건 세션 정보에 접근 할 수 있게 해준다.
+사용예제) ${pincipal.username}
+--%>
+
 <!DOCTYPE html>
 <html lang="en">
 
