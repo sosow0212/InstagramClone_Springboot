33934c2b354c2a98d3c6a1cb4bbd32e59e79c8b9
sosow0212 <sosow0212@naver.com>
2021-12-11 20:33:34 +0900

    구독 - 모델 만들기 (DB)

---
+++ src/main/java/com/cos/photogramstart/domain/user/subscribe/SubscribeRepository.java
@@ -0,0 +1,6 @@
+package com.cos.photogramstart.domain.user.subscribe;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscribeRepository extends JpaRepository<Subscribe, Integer> {
+}
