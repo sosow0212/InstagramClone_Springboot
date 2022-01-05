9a2cd4c37d7dd625a567cda7aecdba4d6223b591
sosow0212 <sosow0212@naver.com>
2022-01-05 16:27:35 +0900

    좋아요 구현 완료 - 오류!!

--- src/main/resources/static/js/story.js
+++ src/main/resources/static/js/story.js
@@ -15,7 +15,6 @@ function storyLoad() {
         url: `/api/image?page=${page}`,
         dataType: "json"
     }).done(res => {
-        console.log(res);
         res.data.content.forEach((image) => {
             let storyItem = getStoryItem(image);
             $("#storyList").append(storyItem);
@@ -47,7 +46,7 @@ function getStoryItem(image) {
                 if(image.likeState) {
                     item += `<i class="fas fa-heart active" id="storyLikeIcon-${image.id}" onclick="toggleLike(${image.id})"></i>`;
                 } else {
-                    item += `<i class="fas fa-heart" id="storyLikeIcon-${image.id}" onclick="toggleLike(${image.id})"></i>`;
+                    item += `<i class="far fa-heart" id="storyLikeIcon-${image.id}" onclick="toggleLike(${image.id})"></i>`;
                 }
         item +=`
             </button>
@@ -97,14 +96,43 @@ $(window).scroll(() => {
 // (3) 좋아요, 안좋아요
 function toggleLike(imageId) {
     let likeIcon = $(`#storyLikeIcon-${imageId}`);
-    if (likeIcon.hasClass("far")) {
-        likeIcon.addClass("fas");
-        likeIcon.addClass("active");
-        likeIcon.removeClass("far");
-    } else {
-        likeIcon.removeClass("fas");
-        likeIcon.removeClass("active");
-        likeIcon.addClass("far");
+
+    if (likeIcon.hasClass("far")) { // 좋아요 하겠다.
+        $.ajax({
+            type:"post",
+            url: `api/image/${imageId}/likes`,
+            dataType: "json"
+        }).done(res=>{
+
+            let likeCountStr = $(`#storyLikeCount-${imageId}`).text();
+            let likeCount = Number(likeCountStr) + 1;
+            $(`#storyLikeCount-${imageId}`).text(likeCount);
+
+            likeIcon.addClass("fas");
+            likeIcon.addClass("active");
+            likeIcon.removeClass("far");
+        }).fail(error=>{
+            console.log("오류", error);
+        });
+
+
+    } else { // 좋아요 취소 하겠다.
+        $.ajax({
+            type:"delete",
+            url: `api/image/${imageId}/likes`,
+            dataType: "json"
+        }).done(res=>{
+
+            let likeCountStr = $(`#storyLikeCount-${imageId}`).text();
+            let likeCount = Number(likeCountStr) - 1;
+            $(`#storyLikeCount-${imageId}`).text(likeCount);
+
+            likeIcon.removeClass("fas");
+            likeIcon.removeClass("active");
+            likeIcon.addClass("far");
+        }).fail(error=>{
+            console.log("오류", error);
+        });
     }
 }
 
